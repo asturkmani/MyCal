df309d63000f08b7fa64b644847d788e1d7e4ddb
asturkmani <asturkmani@gmail.com>
2015-04-17 01:46:29 +0300

    included emailing option

---
+++ projecttrial1/SendEmail.java
@@ -0,0 +1,65 @@
+package projecttrial1;
+
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class SendEmail
+{
+	public  SendEmail()
+	{
+		
+	
+	}
+
+public static void sendGMail (String [] args)
+{    
+    // Sender's email ID needs to be mentioned
+     String from = "asturkmani@gmail.com";
+     String pass ="joumana!?!";
+    // Recipient's email ID needs to be mentioned.
+   String to = "ast11@mail.aub.edu";
+
+   String host = "smtp.gmail.com";
+
+   // Get system properties
+   Properties properties = System.getProperties();
+   // Setup mail server
+   properties.put("mail.smtp.starttls.enable", "true");
+   properties.put("mail.smtp.host", host);
+   properties.put("mail.smtp.user", from);
+   properties.put("mail.smtp.password", pass);
+   properties.put("mail.smtp.port", "587");
+   properties.put("mail.smtp.auth", "true");
+
+   // Get the default Session object.
+   Session session = Session.getDefaultInstance(properties);
+
+   try{
+      // Create a default MimeMessage object.
+      MimeMessage message = new MimeMessage(session);
+
+      // Set From: header field of the header.
+      message.setFrom(new InternetAddress(from));
+
+      // Set To: header field of the header.
+      message.addRecipient(Message.RecipientType.TO,
+                               new InternetAddress(to));
+
+      // Set Subject: header field
+      message.setSubject("This is the Subject Line!");
+
+      // Now set the actual message
+      message.setText("This is actual message");
+
+      // Send message
+      Transport transport = session.getTransport("smtp");
+      transport.connect(host, from, pass);
+      transport.sendMessage(message, message.getAllRecipients());
+      transport.close();
+      System.out.println("Sent message successfully....");
+   }catch (MessagingException mex) {
+      mex.printStackTrace();
+   }
+
+}}
